f7f6ce4a43850323e09796849594a1336235ec74
Fabian Tan <fnever520@git.com>
2019-04-19 01:38:52 +0900

    commit2

--- hw2/read_arff.py
+++ hw2/read_arff.py
@@ -1,11 +1,30 @@
 from scipy.io import arff
 import pandas as pd
 import glob
+from os.path import join,dirname
 
-for file in glob.glob("*.arff"):
-	filename = file.split(".")[0]
-	csvfilename = filename + ".csv"
+# for file in glob.glob("*.arff"):
+#     filename = file.split(".")[0]
+#     csvfilename = filename + ".csv"
 
-	data = arff.loadarff(file)
-	df = pd.DataFrame(data[0])
-	df.to_csv(csvfilename)
+#     data = arff.loadarff(file)
+#     df = pd.DataFrame(data[0])
+#     df.to_csv(csvfilename)
+
+dataset_path = join((dirname(__file__)), "HW2_data")
+data = join(dataset_path,"vote.numeric.arff")
+
+for file in glob.glob(data):
+    filename = file.split(".")[0]
+    csvfilename = filename + ".csv"
+
+    data = arff.loadarff(file)
+    df = pd.DataFrame(data[0])
+    df = df.replace({ 
+                      b'democrat'   : 1,  \
+                      b'republican': 0 
+                    })
+    print(list(df.shape)[0])
+    df.to_csv(csvfilename)
+
+
