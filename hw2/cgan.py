1deb60e1a33797d9478d9fce2d92845e7c3cc76c
Fabian Tan <fnever520@git.com>
2019-04-19 02:23:35 +0900

    commit

---
+++ hw2/cgan.py
@@ -0,0 +1,177 @@
+from __future__ import print_function, division
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply
+from keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+from sklearn.neural_network import MLPClassifier
+import matplotlib.pyplot as plt
+import numpy as np
+
+mlp_classifier = MLPClassifier(hidden_layer_size = [100,100], batch_size = 50)
+mlp.classifier.fit()
+
+class CGAN():
+    def __init__(self):
+        # Input shape
+        self.rows = 1
+        self.cols = 9
+        self.channels = 1
+        self.data_shape = (self.rows, self.cols, self.channels)
+        self.num_classes = 2
+        self.latent_dim = 100
+
+        optimizer = Adam(0.0002, 0.5)
+
+        # Build and compile the discriminator
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss=['binary_crossentropy'],
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build the generator
+        self.generator = self.build_generator()
+
+        # The generator takes noise and the target label as input
+        # and generates the corresponding digit of that label
+        noise = Input(shape=(self.latent_dim,))
+        label = Input(shape=(1,))
+        data = self.generator([noise, label])
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The discriminator takes generated image as input and determines validity
+        # and the label of that image
+        valid = self.discriminator([data, label])
+
+        # The combined model  (stacked generator and discriminator)
+        # Trains generator to fool discriminator
+        self.combined = Model([noise, label], valid)
+        self.combined.compile(loss=['binary_crossentropy'],
+            optimizer=optimizer)
+
+    def build_generator(self):
+
+        model = Sequential()
+
+        model.add(Dense(256, input_dim=self.latent_dim))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(1024))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(np.prod(self.data_shape), activation='tanh'))
+        model.add(Reshape(self.data_shape))
+
+        model.summary()
+
+        noise = Input(shape=(self.latent_dim,))
+        label = Input(shape=(1,), dtype='int32')
+        label_embedding = Flatten()(Embedding(self.num_classes, self.latent_dim)(label))
+
+        model_input = multiply([noise, label_embedding])
+        data = model(model_input)
+
+        return Model([noise, label], data)
+
+    def build_discriminator(self):
+
+        model = Sequential()
+
+        model.add(Dense(512, input_dim=np.prod(self.data_shape)))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.4))
+        model.add(Dense(512))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.4))
+        model.add(Dense(1, activation='sigmoid'))
+        model.summary()
+
+        data = Input(shape=self.data_shape)
+        label = Input(shape=(1,), dtype='int32')
+
+        label_embedding = Flatten()(Embedding(self.num_classes, np.prod(self.data_shape))(label))
+        flat_data = Flatten()(data)
+
+        model_input = multiply([flat_data, label_embedding])
+
+        validity = model(model_input)
+
+        return Model([data, label], validity)
+
+    def train(self, epochs, batch_size=128, sample_interval=50):
+
+        # Load the dataset
+        (X_train, y_train), (_, _) = mnist.load_data()
+
+        # Configure input
+        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+        X_train = np.expand_dims(X_train, axis=3)
+        y_train = y_train.reshape(-1, 1)
+
+        # Adversarial ground truths
+        valid = np.ones((batch_size, 1))
+        fake = np.zeros((batch_size, 1))
+
+        for epoch in range(epochs):
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            # Select a random half batch of images
+            idx = np.random.randint(0, X_train.shape[0], batch_size)
+            datas, labels = X_train[idx], y_train[idx]
+
+            # Sample noise as generator input
+            noise = np.random.normal(0, 1, (batch_size, 100))
+
+            # Generate a half batch of new images
+            gen_datas = self.generator.predict([noise, labels])
+
+            # Train the discriminator
+            d_loss_real = self.discriminator.train_on_batch([datas, labels], valid)
+            d_loss_fake = self.discriminator.train_on_batch([gen_datas, labels], fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+
+            # Condition on labels
+            sampled_labels = np.random.randint(0, 10, batch_size).reshape(-1, 1)
+
+            # Train the generator
+            g_loss = self.combined.train_on_batch([noise, sampled_labels], valid)
+
+            # Plot the progress
+            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+
+            # If at save interval => save generated image samples
+            if epoch % sample_interval == 0:
+                self.sample_images(epoch)
+
+    def sample_data(self, epoch):
+        r = 1
+        noise = np.random.normal(0, 1, (r * c, 100))
+        sampled_labels = np.arange(0, 10).reshape(-1, 1)
+
+        gen_datas = self.generator.predict([noise, sampled_labels])
+
+        # Rescale images 0 - 1
+        gen_datas = 0.5 * gen_datas + 0.5
+
+        return gen_datas
+
+if __name__ == '__main__':
+    cgan = CGAN()
+    cgan.train(epochs=20000, batch_size=32, sample_interval=200)
